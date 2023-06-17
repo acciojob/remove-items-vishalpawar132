463558b492ac9a4172575d8d7dcf3c0bbe75ec34
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-17 21:06:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// script.js
+window.onload = function() {
+  // Get the button and dropdown elements
+  var button = document.querySelector("input[type='button']");
+  var colorSelect = document.getElementById("colorSelect");
+
+  // Add a click event listener to the button
+  button.addEventListener("click", function() {
+    // Get the selected option index
+    var selectedOptionIndex = colorSelect.selectedIndex;
+
+    // Remove the selected option from the dropdown
+    colorSelect.remove(selectedOptionIndex);
+  });
+};
